4179a7a53ae2c34805eb6283c47be7a9b0aee191
bin0m <levitg@gmail.com>
2018-08-02 18:11:17 +0300

    Simple Web Get Retry

--- WebClientNetCore/Program.cs
+++ WebClientNetCore/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
 
 namespace WebClientNetCore
 {
@@ -6,7 +8,18 @@ namespace WebClientNetCore
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Start");
+            HttpClient httpClient = new HttpClient();
+            string message = "no message";
+
+            var maxRetryAttempts = 3;
+            var pauseBetweenFailures = TimeSpan.FromSeconds(5);
+            SimpleRetryHelper.RetryOnException(maxRetryAttempts, pauseBetweenFailures, () => {
+                message = httpClient.GetStringAsync("http://teachmetest.azurewebsites.net/api/values").Result;
+            });
+
+            Console.WriteLine($"Got message:{message}");
+            Console.ReadLine();
         }
     }
 }
---
+++ WebClientNetCore/SimpleRetryHelper.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace WebClientNetCore
+{
+    public class SimpleRetryHelper
+    {
+        public static void RetryOnException(int times, TimeSpan delay, Action operation)
+        {
+            var attempts = 0;
+            do
+            {
+                try
+                {
+                    attempts++;
+                    operation();
+                    break; // Sucess! Lets exit the loop!
+                }
+                catch (Exception ex)
+                {
+                    if (attempts == times)
+                        throw;
+
+                    //todo: write to Log
+                    Console.WriteLine($"Exception caught on attempt {attempts} - will retry after delay {delay}", ex);
+
+                    Task.Delay(delay).Wait();
+                }
+            } while (true);
+        }
+    }
+}
