4138e275a201eed5437b02df9f34db28f75a183d
bin0m <levitg@gmail.com>
2018-08-02 18:30:02 +0300

    Added NLog and DependencyInjection

--- WebClientNetCore/Program.cs
+++ WebClientNetCore/Program.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using NLog.Extensions.Logging;
+using System;
 using System.Net.Http;
 using System.Threading.Tasks;
 
@@ -6,8 +9,37 @@ namespace WebClientNetCore
 {
     class Program
     {
+        private static IServiceProvider BuildDi()
+        {
+            var services = new ServiceCollection();
+
+            //Runner is the custom class
+            services.AddTransient<Runner>();
+
+            services.AddSingleton<ILoggerFactory, LoggerFactory>();
+            services.AddSingleton(typeof(ILogger<>), typeof(Logger<>));
+            services.AddLogging((builder) => builder.SetMinimumLevel(LogLevel.Trace));
+
+            var serviceProvider = services.BuildServiceProvider();
+
+            var loggerFactory = serviceProvider.GetRequiredService<ILoggerFactory>();
+
+            //configure NLog
+            loggerFactory.AddNLog(new NLogProviderOptions { CaptureMessageTemplates = true, CaptureMessageProperties = true });
+            NLog.LogManager.LoadConfiguration("nlog.config");
+
+            return serviceProvider;
+        }
+
         static void Main(string[] args)
         {
+            // initializations
+            var servicesProvider = BuildDi();
+            var runner = servicesProvider.GetRequiredService<Runner>();
+
+            // test logging 
+            runner.DoAction("Action1");
+
             Console.WriteLine("Start");
             HttpClient httpClient = new HttpClient();
             string message = "no message";
@@ -20,6 +52,9 @@ namespace WebClientNetCore
 
             Console.WriteLine($"Got message:{message}");
             Console.ReadLine();
+
+            // Ensure to flush and stop internal timers/threads before application-exit (Avoid segmentation fault on Linux)
+            NLog.LogManager.Shutdown();
         }
     }
 }
